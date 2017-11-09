9e20ba2d630b25bc6ea96e35643c5742ee0aff7a
dg68184 <damian.gancarek@gmail.com>
2017-11-09 15:07:40 +0100

    added conditions

---
+++ src/pl/waw/sgh/BowlingGame.java
@@ -0,0 +1,16 @@
+package pl.waw.sgh;
+
+import java.util.Scanner;
+
+public class BowlingGame {
+        public static void main(String[] args) {
+            System.out.println(" Please provide your number of rows ");
+            int n = new Scanner(System.in).nextInt();
+            int b=0;
+            for (int i = 1; i <= n; i++) {
+                b=b+i;
+            }
+            System.out.println(b);
+        }
+    }
+}
