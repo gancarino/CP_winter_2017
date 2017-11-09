450dc4ecfe9a3f31331735c673cee72add752a56
dg68184 <damian.gancarek@gmail.com>
2017-11-09 11:36:08 +0100

    added conditions

--- src/pl/waw/sgh/EmailCheck.java
+++ src/pl/waw/sgh/EmailCheck.java
@@ -7,20 +7,27 @@ import java.util.Scanner;
 public class EmailCheck {
 
     public static void main(String[] args) {
-        final String EMAIL_OK = "user@sgh.pl";
-        final String EMAIL_NOT_OK = "@sgh.pl";
 
-        String email = EMAIL_NOT_OK;
 
         System.out.println(" Please provide your email ");
         String input = new Scanner(System.in).nextLine();
         System.out.println(("got: " + input));
 
         int atPos = input.indexOf("@");
+        int lz = input.length();
+        String Posi="";
+        boolean popr = false;
+        for(int i=0;i<lz;i++) {
+            char z=input.charAt(i);
+            System.out.println(z);
+            if (input.charAt(i) == '.' && (i > atPos + 1)) {
+                Posi = Posi + i + ";";
+                popr = true;
+            }
+        }
         int dotPos = input.indexOf(".");
         System.out.println("at position = " + atPos);
-        System.out.println("dot position = " + dotPos);
-        if (atPos>=2 && (dotPos > atPos+1) ) {
+        if (atPos>=2 && (popr) ) {
             System.out.println("EMAIL_OK ");
         }
         else {
@@ -28,4 +35,5 @@ public class EmailCheck {
         }
 
     }
+
 }
